057f62db95c0ae46e80860494da37ed47d9718e5
PRAJIN PRAKASH <prajin@techentsia.com>
2020-03-28 19:34:41 +0530

    Removed unwanted files and contents

--- pages/index.vue
+++ pages/index.vue
@@ -3,53 +3,25 @@
     <v-flex xs12 sm8 md6>
       <div class="text-center">
         <logo />
-        <vuetify-logo />
       </div>
       <v-card>
         <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
+          Welcome to {{ 'talent tester' }}
         </v-card-title>
         <v-card-text>
           <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum,
+            nisi? Voluptatum possimus repellendus, iste quas corrupti ducimus
+            fugit alias? Saepe impedit cupiditate facilis explicabo rerum vitae
+            quasi recusandae modi tenetur?
           </p>
+
           <p>
-            For more information on Vuetify, check out the
-            <a href="https://vuetifyjs.com" target="_blank"> documentation </a>.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum
+            voluptatem accusamus veritatis, quo sequi molestias, dolor velit
+            eius excepturi facere rerum impedit magni laudantium minus ducimus
+            amet delectus reprehenderit facilis.
           </p>
-          <p>
-            If you have questions, please join the official
-            <a href="https://chat.vuetifyjs.com/" target="_blank" title="chat">
-              discord </a
-            >.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              title="contribute"
-            >
-              issue board </a
-            >.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3" />
-          <a href="https://nuxtjs.org/" target="_blank">
-            Nuxt Documentation
-          </a>
-          <br />
-          <a href="https://github.com/nuxt/nuxt.js" target="_blank">
-            Nuxt GitHub
-          </a>
         </v-card-text>
         <v-card-actions>
           <v-spacer />
@@ -64,12 +36,10 @@
 
 <script>
 import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
 
 export default {
   components: {
-    Logo,
-    VuetifyLogo
+    Logo
   }
 }
 </script>
